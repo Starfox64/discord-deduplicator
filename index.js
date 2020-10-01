0f8bf5d306b5dd5d7b9530a30a13344c622bc63b
Starfox64 <louisdijon21@yahoo.fr>
2020-10-01 10:03:33 +0200

    Fix empty duplucated file when saving with firefox

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const cursedFilenameRegex = /(image|video)+\d/;
 
 const watchedPaths = process.env.DISCORD_WATCHED_PATHS.split(',').map(s => s.trim());
 
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
 chokidar.watch(watchedPaths, {
   ignoreInitial: true,
   depth: 0
@@ -22,6 +26,9 @@ chokidar.watch(watchedPaths, {
 
   if (cursedFilenameRegex.test(pathData.name)) {
     console.log(`Cursed filename found at ${filePath}, renaming to ${newPath}`);
+
+    // We wait some extra time because it seems firefox does some shenanigans with the file
+    await sleep(150);
 
     try {
       backOff(() => fs.rename(filePath, newPath));
